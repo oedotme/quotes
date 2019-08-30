14ccd83574320865cc2060bde14c399607bb2cfb
iixixv <iixixv.me@gmail.com>
2019-08-30 19:15:21 +0200

    pass fcc tests

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -7,13 +7,18 @@ import styles from './styles.css'
 
 const Quote = ({ quote, author, onClick }) => {
 	return (
-		<main className={styles.quote}>
+		<main className={styles.quote} id="quote-box">
 			<div className={styles.quote__content}>
-				<h1 className={styles.quote__text}>{quote}</h1>
+				<h1 className={styles.quote__text} id="text">
+					{quote}
+				</h1>
 				<div className={styles.quote__more}>
-					<p className={styles.quote__author}> - {author}</p>
+					<p className={styles.quote__author} id="author">
+						- {author}
+					</p>
 					<a
 						className={styles.quote__tweet}
+						id="tweet-quote"
 						href={`https://twitter.com/intent/tweet?hashtags=quotes&text="${quote}" ${author}`}
 						target="_blank"
 					>
@@ -22,8 +27,12 @@ const Quote = ({ quote, author, onClick }) => {
 				</div>
 			</div>
 
-			<button className={styles.quote__new} onClick={onClick}>
-				<img src={QuotesIcon} alt="new quote" />
+			<button className={styles.quote__new} id="new-quote" onClick={onClick}>
+				<img
+					className={styles.quote__new__icon}
+					src={QuotesIcon}
+					alt="new quote"
+				/>
 			</button>
 		</main>
 	)
